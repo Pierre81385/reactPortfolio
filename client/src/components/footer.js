661adb89c37ce9acb81de33e9e9c8e8bb5ef5a89
Peter Bishop <pjb.den@gmail.com>
2021-06-22 18:33:41 -0600

    added assets from previous portfolio, and updated footer with working links

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,9 +1,51 @@
 import React from "react";
 
+const style = {
+  ul: {
+    "text-align": "center",
+  },
+  li: {
+    display: "inline",
+    margin: "2px",
+  },
+};
+
 function Footer() {
   return (
     <footer className="footer">
-      <h1>Footer</h1>
+      <section id="contactMe">
+        <table>
+          <tr>
+            <td>
+              <ul style={style.ul}>
+                <li style={style.li}>
+                  <a href="tel:7209386873" alt="phoneNumber">
+                    Phone
+                  </a>
+                </li>
+                <li style={style.li}>
+                  <a
+                    href="https://www.linkedin.com/in/peter-bishop-46a51597/"
+                    alt="LinkedIn"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+                <li style={style.li}>
+                  <a href="https://github.com/Pierre81385" alt="GitHub">
+                    GitHub
+                  </a>
+                </li>
+                <li style={style.li}>
+                  <a href="https://www.instagram.com/pjb.den/" alt="Instagram">
+                    Instagram
+                  </a>
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </table>
+      </section>
     </footer>
   );
 }
