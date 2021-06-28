2e58d6b87d2b12a9b482ceda9501cb76b945c729
Peter Bishop <pjb.den@gmail.com>
2021-06-27 19:13:46 -0600

    flex cards in a row on portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import CardColumns from "react-bootstrap/CardColumns";
 
 function Portfolio() {
   const projectInfo = [
@@ -56,33 +58,47 @@ function Portfolio() {
 
   const renderCard = (card) => {
     return (
-      <Card className="projectCard" style={{ width: "18rem" }} key={card.id}>
+      <Card className="card" style={{ width: "250px", flex: 1 }} key={card.id}>
         <Card.Img variant="top" src={card.image} />
         <Card.Body>
           <Card.Title>{card.title}</Card.Title>
           <Card.Text>{card.desc}</Card.Text>
-          <a
-            href={card.deploy}
-            class="btn btn-primary btn-lg active"
-            role="button"
-            aria-pressed="true"
-          >
-            Deploy
-          </a>
-          <a
-            href={card.github}
-            class="btn btn-primary btn-lg active"
-            role="button"
-            aria-pressed="true"
-          >
-            Github
-          </a>
+          <footer>
+            <a
+              href={card.deploy}
+              class="btn btn-primary btn-lg active"
+              role="button"
+              aria-pressed="true"
+            >
+              Deploy
+            </a>
+            <a
+              href={card.github}
+              class="btn btn-primary btn-lg active"
+              role="button"
+              aria-pressed="true"
+            >
+              Github
+            </a>
+          </footer>
         </Card.Body>
       </Card>
     );
   };
 
-  return <div className="flex">{projectInfo.map(renderCard)}</div>;
+  return (
+    <CardColumns
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        width: "100%",
+        columnCount: 3,
+      }}
+    >
+      {projectInfo.map(renderCard)}
+    </CardColumns>
+  );
 }
 
 export default Portfolio;
