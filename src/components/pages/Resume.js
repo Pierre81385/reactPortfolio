425dc4706f3832da8928545df8b81ee47312846b
Peter Bishop <pjb.den@gmail.com>
2021-06-28 14:35:20 -0600

    fixed pdf view layout

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import Button from "react-bootstrap/Button";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume() {
@@ -8,6 +10,18 @@ function Resume() {
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
   }
 
   const style = {
@@ -20,20 +34,47 @@ function Resume() {
       paddingTop: "30px",
       paddingLeft: "30px",
     },
+    a: {
+      color: "white",
+      paddingTop: "10px",
+      paddingBottom: "10px",
+    },
   };
 
   return (
     <div style={style.div}>
-      <div>
-        <a href="../PeterBishop_Resume2021.pdf">DOWNLOAD</a>
-      </div>
       <Document
         file="../PeterBishop_Resume2021.pdf"
         onLoadSuccess={onDocumentLoadSuccess}
       >
-        {[1, 2].map((page) => (
-          <Page pageNumber={page} style={{ size: "A4" }} />
-        ))}
+        {/* {[1, 2].map((page) => (
+          <Page pageNumber={page} scale=".75" />
+        ))} */}
+        <div>
+          <a href="../PeterBishop_Resume2021.pdf" style={style.a}>
+            DOWNLOAD
+          </a>
+        </div>
+        <Page pageNumber={pageNumber} scale=".65" />
+        <div style={{ position: "absolute", bottom: "25px" }}>
+          <p>Page {pageNumber}</p>
+          <Button
+            type="button"
+            variant="outline-light"
+            disabled={pageNumber <= 1}
+            onClick={previousPage}
+          >
+            Previous
+          </Button>
+          <Button
+            type="button"
+            variant="outline-light"
+            disabled={pageNumber >= numPages}
+            onClick={nextPage}
+          >
+            Next
+          </Button>
+        </div>
       </Document>
     </div>
   );
